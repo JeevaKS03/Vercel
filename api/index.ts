c05201280b6d0e667a045b92a24e0fb7fd03fcd1
JeevaKS03 <145172065+JeevaKS03@users.noreply.github.com>
2025-01-05 20:31:33 +0530

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 // Middleware and routes
-app.get('/about', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
-
+app.listen(3000, () => console.log("Server ready on port 3000."));
 // Export the app as a handler for Vercel
 module.exports = app;
