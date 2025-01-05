5c7c16ee5f48a95de4a57689230686f1ee4e9d85
JeevaKS03 <145172065+JeevaKS03@users.noreply.github.com>
2025-01-05 19:06:55 +0530

    Update index.js

--- testapp/api/index.js
+++ testapp/api/index.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const app = express();
+
+// Middleware and routes
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Export the app as a handler for Vercel
+module.exports = (req, res) => {
+  app(req, res);
+};
 
